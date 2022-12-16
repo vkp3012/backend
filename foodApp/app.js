5ac1549685672b8781aad22db14d6beb3dc8bdac
vkp3012 <vivekpatel301295@gmail.com>
2022-12-16 12:24:27 +0530

    mongoose get and post is done

---
+++ foodApp/app.js
@@ -0,0 +1,155 @@
+
+const express = require("express")
+const app = express()
+const mongoose = require('mongoose')
+const db_link = require('./secrets')
+app.use(express.json());
+
+let user = [
+    {
+        id:1,
+        name: "Vivek",
+        age:100
+    },
+    {
+        id:2,
+        name: "Lalu",
+        age:10
+    },
+    {
+        id:1,
+        name: "Sonu",
+        age:150
+    },
+];
+
+const userRouter = express.Router()
+const authRouter = express.Router()
+
+app.use("/user",userRouter);
+app.use("/auth",authRouter);
+
+userRouter
+  .route("/")
+  .get(getUsers)
+  .post(postUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+
+userRouter.route("/:name").get(getUserById);
+
+authRouter.route("/signup").get(getSignup).post(postSignup);
+
+async function getUsers(req,res){
+    console.log(req.query);
+    let {name,age} = req.query;
+    //get all users from data base...
+    let allUsers = await userModel.find();
+    res.json({message: "users retrived" , allUsers});
+}
+
+function postUser(req,res){
+    console.log(req.body.Name);
+    user.push(req.body);
+    res.json({
+        message: "Data received sucessfully",
+        user: req.body,
+    })
+}
+
+function updateUser(req,res){
+    console.log(req.body);
+    let dataToBeUpdated = req.body;
+    for(key in dataToBeUpdated){
+        user[key] = dataToBeUpdated[key];
+    }
+    res.json({
+        message: "Data updated Succesfully"
+    });
+}
+
+function deleteUser(req,res){
+    user = {};
+    res.json({
+        message: "user has been deleted",
+    });
+}
+
+
+function getUserById(req, res) {
+    console.log(req.params.name);
+    //let {id}=req.params;
+    // let user = db.findOne(id);
+    res.json({ msg: "user id is ", obj: req.params });
+}
+
+function getSignup(req, res) {
+    res.sendFile("/public/index.html", { root: __dirname });
+}
+
+async function postSignup(req, res) {
+    // let { email, name, password } = req.body;
+    try {
+        let data = req.body;
+        let user = await userModel.create(data)
+        console.log(data);
+        res.json({
+            msg: "user signed up",
+            // email,
+            // name,
+            // password
+            user
+        })
+    }
+    catch (err){
+        res.json({
+            err:err.message
+        })
+    }
+    
+}
+  
+
+app.listen(5000)
+
+mongoose.connect(db_link)
+    .then(function(db){
+        console.log("db connected");
+        // console.log(db);
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    confirmpassword:{
+        type:String,
+        required:true,
+    },
+})
+
+const userModel = mongoose.model("userModel",userSchema);
+
+// (async function createUser() {
+//     let user = {
+//         name:"Versha",
+//         email:"Versha@gmail.com",
+//         password : "12345678",
+//         confirmpassword :"12345678"
+//     };
+//     let data = await userModel.create(user);
+//     console.log(data) ;
+// })() 
